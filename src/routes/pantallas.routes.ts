a28083df7c51b3d170bd06ffd61a2943538fcad3
ApiMeatze <oscargm28@hotmail.com>
2020-12-22 13:08:03 +0100

    commit 22 del 12 de 2020

--- src/routes/pantallas.routes.ts
+++ src/routes/pantallas.routes.ts
@@ -5,8 +5,8 @@ import isValidToken from '../middlewares/validateToken';
 import {isValidRole} from '../middlewares/validateRole';
 /*Cuando se llama a un archivo se puede traer sin destructurar solo la que este por default */
 
-router.get('/pantallas', isValidToken, pantallasController.getPantallas);
-router.get('/pantallas/:id', isValidToken, pantallasController.getPantalla);
+router.get('/pantallas', /*isValidToken,*/ pantallasController.getPantallas);
+router.get('/pantallas/:id',/* isValidToken,*/ pantallasController.getPantalla);
 router.post('/pantallas', isValidToken, isValidRole, pantallasController.createPantalla);
 router.put('/pantallas/:id', isValidToken, pantallasController.updatePantalla);
 router.delete('/pantallas/:id', isValidToken,  isValidRole, pantallasController.deletePantalla);
